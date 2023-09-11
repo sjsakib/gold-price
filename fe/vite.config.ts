a0e383081e7664c490a828b836b614d83fc4b18e
Sakib <sjsakib.bd@gmail.com>
2023-09-12 02:48:39 +0600

    auto update service worker

--- fe/vite.config.ts
+++ fe/vite.config.ts
@@ -1,30 +1,31 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import dsv from '@rollup/plugin-dsv';
-// import { VitePWA } from 'vite-plugin-pwa';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     dsv(),
-    // VitePWA({
-    //   workbox: {
-    //     skipWaiting: true,
-    //   },
-    //   manifest: {
-    //     name: 'Gold Price History in Bangladesh',
-    //     short_name: 'GoldPriceBD',
-    //     theme_color: '#FFD700',
-    //     icons: [
-    //       {
-    //         src: 'coin-512.png',
-    //         sizes: '512x512',
-    //         type: 'image/png',
-    //         purpose: 'any maskable'
-    //       },
-    //     ],
-    //   },
-    // }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        skipWaiting: true,
+      },
+      manifest: {
+        name: 'Gold Price History in Bangladesh',
+        short_name: 'GoldPriceBD',
+        theme_color: '#FFD700',
+        icons: [
+          {
+            src: 'coin-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
   ],
 });
