667fdf7e3b8071abda02dd4caff3965c62c47b4c
Sakib <sjsakib.bd@gmail.com>
2023-09-12 00:29:39 +0600

    installability update

--- fe/vite.config.ts
+++ fe/vite.config.ts
@@ -1,8 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 import dsv from '@rollup/plugin-dsv';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dsv()],
-})
+  plugins: [
+    react(),
+    dsv(),
+    VitePWA({
+      manifest: {
+        name: 'Gold Price History in Bangladesh',
+        short_name: 'GoldPriceBD',
+        theme_color: '#FFD700',
+        icons: [
+          {
+            src: 'coin-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+        ],
+      },
+    }),
+  ],
+});
